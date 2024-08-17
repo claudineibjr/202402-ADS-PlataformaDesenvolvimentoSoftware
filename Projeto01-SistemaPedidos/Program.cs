61a7ec6c5cd1a6ce00aea5f5c0497fa9e5247f55
ClaudineiBJr <claudineibjr@hotmail.com>
2024-08-17 10:40:10 -0300

    Inclusão do Swagger

--- Projeto01-SistemaPedidos/Program.cs
+++ Projeto01-SistemaPedidos/Program.cs
@@ -5,7 +5,19 @@ namespace Projeto01_SistemaPedidos
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Configuração do Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                // Inicialização do Swagger
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.MapGet("/", () => "Hello World!");
 
