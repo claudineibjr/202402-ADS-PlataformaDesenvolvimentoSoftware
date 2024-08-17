6e20da17b33b7d130b20966164ec0a462e27d6b3
ClaudineiBJr <claudineibjr@hotmail.com>
2024-08-17 12:13:23 -0300

    Implementação de controllers/models/repositories

---
+++ Projeto01-SistemaPedidos/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace Projeto01_SistemaPedidos.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
---
+++ Projeto01-SistemaPedidos/Repositories/PedidosDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto01_SistemaPedidos.Models;
+
+namespace Projeto01_SistemaPedidos.Repositories;
+
+public partial class PedidosDbContext : DbContext
+{
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Pedido> Pedidos { get; set; }
+
+    public PedidosDbContext()
+    {
+    }
+
+    public PedidosDbContext(DbContextOptions<PedidosDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
