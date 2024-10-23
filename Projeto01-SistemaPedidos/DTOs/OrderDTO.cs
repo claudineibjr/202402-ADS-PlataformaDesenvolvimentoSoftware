491c50e14b928d2a21891a8d4bee46764d32d74a
Claudinei B Jr <claudineibjr@hotmail.com>
2024-10-22 22:07:08 -0300

    Moving the backend projecto to the OrdersManager-Backend folder

--- Projeto01-SistemaPedidos/DTOs/OrderDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Projeto01_OrdersManager.DTOs
-{
-    public class OrderDTO
-    {
-        public string CustomerId { get; set; }
-        public ICollection<ProductItemDTO> Products { get; set; }
-    }
-}
--- Projeto01-SistemaPedidos/DTOs/ProductItemDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Projeto01_OrdersManager.DTOs
-{
-    public class ProductItemDTO
-    {
-        public string ProductId { get; set; }
-        public double Quantity { get; set; }
-    }
-}
