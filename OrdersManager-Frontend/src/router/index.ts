a75a5d82acba0edccd7a068262ca9b5d9377ef03
Claudinei B Jr <claudineibjr@hotmail.com>
2024-10-28 17:28:17 -0300

    Frontend: Reorganizing Login and Orders views

--- OrdersManager-Frontend/src/router/index.ts
+++ OrdersManager-Frontend/src/router/index.ts
@@ -1,21 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '@/views/AboutView.vue'
+import authRoutes from './authRoutes';
+import LoginView from '@/views/LoginView.vue';
+import OrdersView from '@/views/OrdersView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: AboutView,
-    }
+      path: '/orders',
+      name: 'orders',
+      component: OrdersView,
+    },
   ]
 })
 
-export default router
+router.beforeEach(authRoutes);
+
+export default router;
