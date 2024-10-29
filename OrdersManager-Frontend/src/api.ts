45eb6a298b6da0fb6faa07b688543f06ec39db54
Claudinei B Jr <claudineibjr@hotmail.com>
2024-10-29 17:39:08 -0300

    Frontend: Implementing getting orders from the backend and sending the authorization header

--- OrdersManager-Frontend/src/api.ts
+++ OrdersManager-Frontend/src/api.ts
@@ -4,4 +4,15 @@ const api = axios.create({
   baseURL: "http://localhost:5149/api",
 });
 
+api.interceptors.request.use((config) => {
+  const authToken = window.localStorage.getItem("AUTH_TOKEN");
+  const isAuthenticated = Boolean(authToken);
+
+  if (isAuthenticated) {
+    config.headers['Authorization'] = `Bearer ${authToken}`;
+  }
+
+  return config;
+});
+
 export default api;
--- OrdersManager-Frontend/src/views/OrdersView.vue
+++ OrdersManager-Frontend/src/views/OrdersView.vue
@@ -1,88 +1,28 @@
 <script setup lang="ts">
+import api from '@/api';
 import OrdersTableHeader from '@/components/OrdersTableHeader.vue';
 import OrdersTableItem from '@/components/OrdersTableItem.vue';
 import type { OrderType } from '@/models/Order';
+import { onMounted, ref } from 'vue';
 
-const orders: OrderType[] = [
-  {
-    "id": "82792d8a-c37b-419e-85f6-60aac56f6b97",
-    "customer": {
-      "name": "Ana Pereira",
-    },
-    "orderDate": new Date("2024-10-28T18:28:16.37105"),
-    "products": [
-      {
-        "product": {
-          "id": "Trakinas",
-          "name": "Biscuits Recheado Trakinas",
-          "price": 4.5,
-        },
-        "quantity": 5
-      },
-      {
-        "product": {
-          "id": "Marilan",
-          "name": "Biscoito Integral Marilan",
-          "price": 5.5,
-        },
-        "quantity": 3
-      },
-      {
-        "product": {
-          "id": "Coca_Cola",
-          "name": "Coca-Cola",
-          "price": 5
-        },
-        "quantity": 1
-      }
-    ],
-    "totalAmount": 44
-  },
-  {
-    "id": "dc100f38-70ce-4c40-9cf0-ef14a708cad7",
-    "customer": {
-      "name": "Ana Pereira",
-    },
-    "orderDate": new Date("2024-10-28T18:28:38.634609"),
-    "products": [
-      {
-        "product": {
-          "id": "Nivea",
-          "name": "Nivea Condicionador",
-          "price": 10.5,
-        },
-        "quantity": 2
-      },
-      {
-        "product": {
-          "id": "Fanta",
-          "name": "Fanta Laranja",
-          "price": 5
-        },
-        "quantity": 3
-      }
-    ],
-    "totalAmount": 36
-  },
-  {
-    "id": "ef4b249d-5267-455f-81bf-1ddb62d60294",
-    "customer": {
-      "name": "Ana Pereira"
-    },
-    "orderDate": new Date("2024-10-28T18:28:50.463012"),
-    "products": [
-      {
-        "product": {
-          "id": "Club_Social",
-          "name": "Club Social Biscoito Salgado",
-          "price": 3.5
-        },
-        "quantity": 10
-      }
-    ],
-    "totalAmount": 35
+const orders = ref<OrderType[]>();
+
+onMounted(async () => {
+  console.log('Buscando pedidos...');
+
+  try {
+    const ordersResult = await api.get<OrderType[]>('/Order');
+
+    console.log({ ordersResult }, '[Orders]');
+
+    orders.value = ordersResult.data;
+
+  } catch (error) {
+    alert("Falha ao buscar pedidos");
+
+    console.error({ error }, 'Falha ao buscar pedidos');
   }
-];
+});
 
 </script>
 
