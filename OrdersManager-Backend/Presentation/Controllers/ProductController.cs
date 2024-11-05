86ea52835e705c2d330bd472eed023304272c453
Claudinei B Jr <claudineibjr@hotmail.com>
2024-11-05 18:09:01 -0300

    Backend: New endpoint POST /product/:productId/UploadImage

--- OrdersManager-Backend/Presentation/Controllers/ProductController.cs
+++ OrdersManager-Backend/Presentation/Controllers/ProductController.cs
@@ -23,5 +23,32 @@ namespace Presentation.Controllers
         public async Task<Product> GetProduct(string ProductId) {
           return await _productService.GetProduct(ProductId);
         }
+
+        [HttpPost("{ProductId}/UploadImage")]
+        public async Task<ActionResult<string>> UploadImage(string ProductId, IFormFile file)
+        {
+          if (file == null || file.Length == 0)
+          {
+              return BadRequest("No image found");
+          }
+
+          string fileExtension = file.FileName.Substring(file.FileName.LastIndexOf(".") + 1);
+
+          string folderName = "products";
+          string productsUploadFolderPath = Path.Combine("wwwroot", folderName);
+          Directory.CreateDirectory(productsUploadFolderPath);
+
+          string fileName = $"{ProductId}.{fileExtension}";
+          string filePath = Path.Combine(productsUploadFolderPath, fileName);
+
+          using (var stream = new FileStream(filePath, FileMode.Create))
+          {
+            await file.CopyToAsync(stream);
+          }
+
+          string imageUrl = $"/{folderName}/{fileName}";
+
+          return CreatedAtAction(nameof(UploadImage), imageUrl);
+        }
     }
 }
